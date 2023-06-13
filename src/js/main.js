d93a0cdae92d2f22839b33a54a2363bd752d5534
Hamdy-Mohamed <121273938+Hamdy-Mohamed@users.noreply.github.com>
2023-06-13 04:54:14 +0300

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,13 @@
 // animation && events
 
-let navi = document.querySelector(".navi");
-
-function naviEvent() {
-  navi.style.background = "#73BBC9";
-  navi.style.opacity = "0.7";
-}
-window.addEventListener("scroll", naviEvent);
+// let mode = document.querySelector(".mode");
+// let home = document.querySelector(".sec-1");
+// let navbar = document.querySelector("nav");
+// mode.onclick = () => {
+//   document.body.classList.toggle("switch");
+//   home.classList.toggle("sec_color");
+//   liNav.classList.toggle("navWords");
+// };
 
 let logo = document.querySelector(".logo");
 logo.onclick = () => {
@@ -14,29 +15,26 @@ logo.onclick = () => {
 };
 
 let about_img = document.querySelector(".about_img");
-
 let secound = 1000;
 let menutes = secound * 60;
 
 about_img.addEventListener("mousemove", () => {
   setTimeout(() => {
-    about_img.style.animation = "rotate3d 2s infinite";
+    about_img.style.animation = "rotate3d 7s infinite";
   }, menutes);
 });
 
-// icons
+// icons events
 
 let facebook = document.querySelector(".facebook");
 let insta = document.querySelector(".insta");
 let github = document.querySelector(".github");
-
 facebook.onclick = () => {
   window.open("https://www.facebook.com/hamdy.elgokar.5");
 };
 insta.onclick = () => {
   window.open("https://www.instagram.com/askander_19/");
 };
-
 github.onclick = () => {
   window.open("https://github.com/Hamdy-Mohamed");
 };
